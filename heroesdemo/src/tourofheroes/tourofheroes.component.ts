d4c17835a0cd8020806bc85b17b6ac2352be8428
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-10-29 22:52:48 +0530

    Added another Card with List of Heroes

--- heroesdemo/src/tourofheroes/tourofheroes.component.ts
+++ heroesdemo/src/tourofheroes/tourofheroes.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 
 import { Hero } from './models/Hero';
+import { HEROES } from './models/Heroes';
 
 @Component({
   selector: 'app-heroesroot',
   templateUrl: './tourofheroes.component.html',
   styleUrls: ['./tourofheroes.component.css']
 })
-
 export class TourOfHeroesComponent {
   title = 'Tour of Heroes';
+  heroes = HEROES;
 
   hero: Hero = {
     id: 1,
