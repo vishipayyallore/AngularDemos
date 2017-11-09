7eb9303a3d251ba0f686c979181d778207c95192
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-11-09 22:13:11 +0530

    ..

--- heroesdemo/src/tourofheroes/tourofheroes.component.ts
+++ heroesdemo/src/tourofheroes/tourofheroes.component.ts
@@ -3,22 +3,31 @@ import { Component } from '@angular/core';
 import { Hero } from './models/Hero';
 import { HEROES } from './data/Heroes';
 import { HeroService } from './services/hero.service';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-heroesroot',
   templateUrl: './tourofheroes.component.html',
   styleUrls: ['./tourofheroes.component.css']
 })
-export class TourOfHeroesComponent {
+export class TourOfHeroesComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroesList = HEROES;
+  heroesList: Hero[] = null;
   selectedHero: Hero;
 
   constructor(private heroService: HeroService) {
   }
 
+  ngOnInit() {
+    this.getHeros();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeros(): void {
+    this.heroesList = this.heroService.getHeros();
   }
 
 }
