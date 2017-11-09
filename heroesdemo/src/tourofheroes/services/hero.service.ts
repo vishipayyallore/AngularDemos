f456484d26b862c75675cead35bfcd3e9865bc38
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-11-09 22:05:29 +0530

    Added getHeros() method.

--- heroesdemo/src/tourofheroes/services/hero.service.ts
+++ heroesdemo/src/tourofheroes/services/hero.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import { Hero } from '../models/Hero';
+import { HEROES } from '../data/Heroes';
+
 @Injectable()
 export class HeroService {
 
     constructor() {
+    }
+
+    public getHeros(): Hero[] {
+        return HEROES;
     }
 
 }
