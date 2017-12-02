338e2452e581e8b8c13fa6c0e2f16d5a7efd00e5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-12-02 23:07:52 +0530

    Few changes to route to individual Hero Details Page.

--- heroesdemo/src/tourofheroes/app-routing.module.ts
+++ heroesdemo/src/tourofheroes/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Router, Routes } from '@angular/router';
 
 import { TourOfHeroesComponent } from './heroeslist/tourofheroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './herodetail/hero-detail.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    { path: 'heroes', component: TourOfHeroesComponent },
     { path: 'dashboard', component: DashboardComponent },
+    { path: 'detail/:id', component: HeroDetailComponent },
+    { path: 'heroes', component: TourOfHeroesComponent },
 ];
 
 @NgModule({
