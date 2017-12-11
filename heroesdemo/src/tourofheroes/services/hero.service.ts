503404657b9db3e2e9901bf1017ff86100909abe
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-12-11 22:56:57 +0530

    Spacing

--- heroesdemo/src/tourofheroes/services/hero.service.ts
+++ heroesdemo/src/tourofheroes/services/hero.service.ts
@@ -20,6 +20,6 @@ export class HeroService {
 
     public getHero(id: number): Observable<Hero> {
         this.messageService.add(`HeroService: fetched hero id=${id}`);
-        return of(HEROES.find( hero => hero.id === id));
+        return of(HEROES.find(hero => hero.id === id));
     }
 }
