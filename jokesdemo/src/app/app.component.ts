2230474ece2756ac7511c5617da75b4123812be0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-01-12 00:09:36 +0530

    Hello World

--- jokesdemo/src/app/app.component.ts
+++ jokesdemo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Jokes Demo Application';
 }
--- jokesdemo/src/app/app.module.ts
+++ jokesdemo/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule, MatCheckboxModule, MatCardModule, MatMenuModule,
+  MatInputModule, MatToolbarModule, MatIconModule, MatSidenavModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +14,17 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
