a50a49120663593b271e8dc673385ad669e5d63a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-01-19 23:53:46 +0530

    Added Mat-Slide-Toggle

--- jokesdemo/src/app/app.module.ts
+++ jokesdemo/src/app/app.module.ts
@@ -1,40 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
-    MatButtonModule, MatCheckboxModule, MatCardModule, MatMenuModule,
-    MatInputModule, MatToolbarModule, MatIconModule, MatSidenavModule,
-    MatSliderModule, MatSlideToggle, MatSlider, MatSlideToggleBase
+  MatButtonModule, MatCheckboxModule, MatCardModule, MatMenuModule,
+  MatInputModule, MatToolbarModule, MatIconModule, MatSidenavModule,
+  MatSlideToggleModule
 } from '@angular/material';
 
-import { AppComponent } from './app.component';
-import { JokeComponent } from './jokes/joke.component';
-import { FooterComponent } from './reusables/footer/footer.component';
-import { JokeListComponent } from './JokeListComponent/jokelist.component';
+import {AppComponent} from './app.component';
+import {JokeComponent} from './jokes/joke.component';
+import {FooterComponent} from './reusables/footer/footer.component';
+import {JokeListComponent} from './JokeListComponent/jokelist.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        JokeComponent,
-        FooterComponent,
-        JokeListComponent
-    ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatButtonModule,
-        MatCheckboxModule,
-        MatInputModule,
-        MatToolbarModule,
-        MatCardModule,
-        MatMenuModule,
-        MatToolbarModule,
-        MatIconModule,
-        MatSidenavModule,
-        MatSliderModule,
-        MatSlideToggleBase
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    JokeComponent,
+    FooterComponent,
+    JokeListComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
