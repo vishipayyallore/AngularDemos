ce084b0cf0f98af321c37e36043b59b467792d7d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-01-19 23:33:07 +0530

    Simple Input Property binding

--- jokesdemo/src/app/JokeListComponent/jokelist.component.ts
+++ jokesdemo/src/app/JokeListComponent/jokelist.component.ts
@@ -13,17 +13,20 @@ export class JokeListComponent {
             {
                 setup: 'Joke 1',
                 punchline: 'Punch for Joke 1',
-                storyline: 'What did the cheese say when it looked in the mirror?'
+                storyline: 'What did the cheese say when it looked in the mirror?',
+                hide: true
             },
             {
                 setup: 'Joke 2',
                 punchline: 'Punch for Joke 2',
-                storyline: 'What kind of cheese do you use to disguise a small horse?'
+                storyline: 'What kind of cheese do you use to disguise a small horse?',
+                hide: true
             },
             {
                 setup: 'Joke 3',
                 punchline: 'Punch for Joke 3',
-                storyline: 'A kid threw a lump of cheddar at me'
+                storyline: 'A kid threw a lump of cheddar at me',
+                hide: true
             }
         ];
 
