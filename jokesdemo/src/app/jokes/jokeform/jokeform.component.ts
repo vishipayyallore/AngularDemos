e756494f8a384a65913f4e583c9d20866b03c145
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-01-27 00:23:33 +0530

    Clearing the form

--- jokesdemo/src/app/jokes/jokeform/jokeform.component.ts
+++ jokesdemo/src/app/jokes/jokeform/jokeform.component.ts
@@ -1,5 +1,6 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { JokeData } from '../../models/joke.data';
+import { MatSlideToggle } from '@angular/material';
 
 @Component({
     selector: 'app-jokeform',
@@ -8,9 +9,21 @@ import { JokeData } from '../../models/joke.data';
 })
 export class JokeFormComponent {
     @Output() jokeCreated = new EventEmitter<JokeData>();
+    @ViewChild('setup') setup: ElementRef;
+    @ViewChild('punchline') punchline: ElementRef;
+    @ViewChild('storyline') storyline: ElementRef;
+    @ViewChild('hide') hide: MatSlideToggle;
 
     createJoke(setup: string, punchline: string, storyline: string, hide: boolean) {
         this.jokeCreated.emit(new JokeData(setup, punchline, storyline, hide));
     }
+
+    clearForm() {
+        this.setup.nativeElement.value = '';
+        this.punchline.nativeElement.value = '';
+        this.storyline.nativeElement.value = '';
+        this.hide.checked = false;
+    }
+
 }
 
