c540834ccd2478c76dafafeff465c48fd03d9063
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-02-05 23:37:39 +0530

    Brought the application back to normal

--- jokesdemo/src/app/jokes/jokeslist/jokelist.component.ts
+++ jokesdemo/src/app/jokes/jokeslist/jokelist.component.ts
@@ -11,13 +11,11 @@ export class JokeListComponent {
     jokes: Array<JokeData> = [];
 
     constructor(public snackBar: MatSnackBar) {
-        /*
         this.jokes = [
             new JokeData('Joke 1', 'Punch for Joke 1', 'What did the cheese say when it looked in the mirror?'),
             new JokeData('Joke 2', 'Punch for Joke 2', 'What kind of cheese do you use to disguise a small horse?'),
             new JokeData('Joke 3', 'Punch for Joke 3', 'A kid threw a lump of cheddar at me')
         ];
-        */
     }
 
     addJoke(joke) {
@@ -25,15 +23,12 @@ export class JokeListComponent {
     }
 
     deleteJoke(joke) {
-        this.jokes = [];
-        /*
         const index = this.jokes.indexOf(joke);
         if (index !== -1) {
             this.jokes.splice(index, 1);
             this.snackBar.open('Joke Deleted!', 'Close', {
                 duration: 2000,
-              });
+            });
         }
-        */
     }
 }
