7acd7250c61e7fbbe3955835552821068ed4be8d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-02-13 16:10:43 +0530

    Changed any to person type

--- jokesdemo/src/app/directivesdemo/ngfordemo/ngfordemo.component.ts
+++ jokesdemo/src/app/directivesdemo/ngfordemo/ngfordemo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Person } from '../../models/person.data';
 
 
 @Component({
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./ngfordemo.component.css']
 })
 export class NgForDemoComponent {
-    people: any[] = [
+    people: Person[] = [
         { 'name': 'Shiva Sai' },
         { 'name': 'Mathew Phlips' },
         { 'name': 'Douglas  Pace' },
