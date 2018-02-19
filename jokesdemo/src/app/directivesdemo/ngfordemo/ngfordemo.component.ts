4923e1ca0433f2e360dc0cf2141af2e123d7eb92
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-02-19 20:57:12 +0530

    Changed the code from ngSwitch to ngStyle

--- jokesdemo/src/app/directivesdemo/ngfordemo/ngfordemo.component.ts
+++ jokesdemo/src/app/directivesdemo/ngfordemo/ngfordemo.component.ts
@@ -22,22 +22,26 @@ export class NgForDemoComponent {
         {
             country: 'UK', peoplesList: [
                 { 'name': 'Shiva Sai', age: 25 },
-                { 'name': 'Mathew Phlips', age: 26 }]
+                { 'name': 'Mathew Phlips', age: 26 }],
+            backGroundColor: 'blue'
         },
         {
             country: 'US', peoplesList: [
                 { 'name': 'Mcleod  Mueller', age: 27 },
-                { 'name': 'Day  Meyers', age: 28 }]
+                { 'name': 'Day  Meyers', age: 28 }],
+            backGroundColor: 'red'
         },
         {
             country: 'HK', peoplesList: [
                 { 'name': 'Aguirre  Ellis', age: 29 },
-                { 'name': 'Cook  Tyson', age: 30 }]
+                { 'name': 'Cook  Tyson', age: 30 }],
+            backGroundColor: 'green'
         },
         {
             country: 'XX', peoplesList: [
                 { 'name': 'XX Aguirre  Ellis', age: 29 },
-                { 'name': 'XX Cook  Tyson', age: 30 }]
+                { 'name': 'XX Cook  Tyson', age: 30 }],
+            backGroundColor: 'orange'
         }
     ];
 
--- jokesdemo/src/app/models/people.data.ts
+++ jokesdemo/src/app/models/people.data.ts
@@ -4,5 +4,6 @@ import { Person } from './person.data';
 export class People {
     country: string;
     peoplesList: Array<Person>;
-
+    // tslint:disable-next-line:no-inferrable-types
+    backGroundColor?: string = 'blue';
 }
