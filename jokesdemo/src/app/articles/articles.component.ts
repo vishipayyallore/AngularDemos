d3750646fed6ebc2967e29cdb6d1e1d849b43113
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-02-22 23:27:43 +0530

    Modified the size of the image.

--- jokesdemo/src/app/articles/articles.component.ts
+++ jokesdemo/src/app/articles/articles.component.ts
@@ -11,11 +11,11 @@ export class ArticlesComponent {
 
     articles: Array<ArticleComponent> = [
         new ArticleComponent('Title 1', new Date(), 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 'text'),
-        new ArticleComponent('Title 2', new Date(), 'https://unsplash.it/400?image=10', 'image'),
+        new ArticleComponent('Title 2', new Date(), 'https://unsplash.it/340?image=10', 'image'),
         new ArticleComponent('Title 3', new Date(), 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 'text'),
-        new ArticleComponent('Title 4', new Date(), 'https://unsplash.it/400?image=20', 'image'),
+        new ArticleComponent('Title 4', new Date(), 'https://unsplash.it/340?image=20', 'image'),
         new ArticleComponent('Title 5', new Date(), 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 'text'),
-        new ArticleComponent('Title 6', new Date(), 'https://unsplash.it/400?image=30', 'image')
+        new ArticleComponent('Title 6', new Date(), 'https://unsplash.it/340?image=30', 'image')
     ];
 
     constructor() { }
