1ce7f94b16d535484ad0b05845634e04c1e43bc1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-04-24 20:49:23 +0530

    Fixed the e2e

--- fruitvendor/e2e/app.e2e-spec.ts
+++ fruitvendor/e2e/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { AppPage } from './app.po';
 
 describe('fruitvendor App', () => {
   let page: AppPage;
+  const pageHeader = 'Welcome to Fruit Vendors!';
 
   beforeEach(() => {
     page = new AppPage();
@@ -9,6 +10,6 @@ describe('fruitvendor App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual(pageHeader);
   });
 });
